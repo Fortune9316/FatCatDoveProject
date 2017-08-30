966001a07f3742047bba1377acb5d1a9985a05b9
carlos montaño rivera <carlos montao rivera>
2017-08-30 16:30:19 -0500

    Scripts and folder structure

---
+++ Assets/Scripts/BonusStage/BonusStageMain.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusStageMain : MonoBehaviour {
+
+    #region States
+    enum BonusStates
+    {
+        BORN,
+        PLAYING,
+        GAMEOVER
+    }
+    #endregion
+    #region public variables
+
+    public static BonusStageMain instance;
+
+    [Header("Notes")]
+    public List<GameObject> notes;
+
+    public Transform spawnPoint;
+
+    [HideInInspector]
+    public float minX;
+    [HideInInspector]
+    public float maxX;
+    [HideInInspector]
+    public float minY;
+    [HideInInspector]
+    public float maxY;
+
+    #endregion
+
+    #region private variables
+
+    private BonusStates bonusStates;
+
+    private float timeElapsed;
+    private float timeToSpawn;
+
+    #endregion
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+
+        //Initialize variables
+        bonusStates = BonusStates.PLAYING;
+        timeElapsed = 0f;
+        timeToSpawn = 0f;
+        setBoundsXY();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+        
+
+        switch (bonusStates)
+        {
+            case BonusStates.BORN:
+                break;
+            case BonusStates.PLAYING:
+                timeToSpawn += Time.deltaTime;
+
+                if(timeToSpawn >= Random.Range(1,3))
+                {
+                    ActiveRandomNote();
+                    timeToSpawn = 0f;
+                }
+
+                for (int i = 0; i < notes.Count; i++)
+                {
+                    if(notes[i].activeInHierarchy)
+                    {
+                        notes[i].GetComponent<NoteController>().UpdateNote();
+                    }
+                }
+                break;
+            case BonusStates.GAMEOVER:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ActiveRandomNote()
+    {
+        for (int i = 0; i < notes.Count; i++)
+        {
+            if (!notes[i].activeInHierarchy)
+            {                
+                notes[i].GetComponent<NoteController>().spawnPoint = spawnPoint.position;
+                Vector3 dirAux = new Vector3(Random.Range(minX, maxX), Random.Range(2, maxY), 0f);
+                dirAux = Vector3.Normalize(dirAux);
+                notes[i].GetComponent<NoteController>().dir = dirAux - spawnPoint.position;
+                notes[i].SetActive(true);
+                break;
+            }
+        }
+    }
+
+    void setBoundsXY()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(
+            Screen.width, Screen.height, 0));
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+        maxY = bounds.y - 0.5f;
+        minY = -bounds.y + 0.5f;
+    }
+}
---
+++ Assets/Scripts/BonusStage/NoteController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour {
+
+    #region States
+
+    #region Public Variables
+
+    public float speed;
+
+    public Vector3 dir;
+    public Vector3 spawnPoint;
+
+    #endregion
+
+    #region Private Variables
+
+    private NoteStates noteStates;
+
+    private RaycastHit raycasthit;
+
+    #endregion
+
+    enum NoteStates
+    {
+        BORN,
+        MOVE,
+        DIE,
+    }
+
+    #endregion
+    // Use this for initialization
+    void Start () {
+        noteStates = NoteStates.BORN;
+        speed = 0.5f;
+	}
+	
+	// Update is called once per frame
+	public void UpdateNote () {
+        switch (noteStates)
+        {
+            case NoteStates.BORN:
+
+                this.transform.position = spawnPoint;
+                noteStates = NoteStates.MOVE;
+
+                break;
+            case NoteStates.MOVE:
+                this.transform.position += dir * speed * Time.deltaTime;
+                print("moving");
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    //if(Physics.Raycast(ray, out raycasthit))
+                    //{
+                    //    if (raycasthit.collider.gameObject.tag == "note")
+                    //    {
+                    //        noteStates = NoteStates.DIE;
+                    //    }
+                    //}
+
+                    print("click");
+
+                    if(Vector3.Distance(transform.position,Camera.main.ScreenToWorldPoint(Input.mousePosition)) <= 0.2f)
+                    {
+                        noteStates = NoteStates.DIE;
+                    }
+                }
+
+                if(transform.position.x > BonusStageMain.instance.maxX || transform.position.x < BonusStageMain.instance.minX || transform.position.y> BonusStageMain.instance.maxY)
+                {
+                    noteStates = NoteStates.DIE;
+                }
+
+                break;
+            case NoteStates.DIE:
+                noteStates = NoteStates.BORN;
+                this.gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
